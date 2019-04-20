3fe5e5f0b95555fa848d2b1751105df50477cee4
NonomiyaYuta <ynonomiya.ocum@gmail.com>
2019-04-20 14:19:54 +0900

    Vue練習中

--- script.js
+++ script.js
@@ -3,16 +3,46 @@ var app1 = new Vue({
     data : {
         books : [
             {
-                title:"book1",
-                class:"class1"
+                title : "book1",
+                class : "class1",
+                author : "author1",
+                price : 1400,
+                isbn : "0001",
+                lectures:[
+                    {
+                        title : "lecture1-1",
+                        youbi : "月",
+                        jigen : "1"
+                    },
+                    {
+                        title : "lecture1-2",
+                        youbi : "火",
+                        jigen : "1"
+                    },
+                    {
+                        title : "lecture1-3",
+                        youbi : "水",
+                        jigen : "1"
+                    },
+                ],
+                isDetail : false
+                
             },
             {
-                title:"book2",
-                class:"class2"
+                title : "book2",
+                class : "class2",
+                author : "author2",
+                price : 1500,
+                isbn : "0002",
+                isDetail : false
             },
             {
-                title:"book3",
-                class:"class3"
+                title : "book3",
+                class : "class3",
+                author : "author3",
+                price : 1600,
+                isbn : "0003",
+                isDetail : false
             }
         ]
     }
