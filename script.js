ee7c1e237dd823580f01661874131f32171efc55
NonomiyaYuta <ynonomiya.ocum@gmail.com>
2019-04-20 10:45:51 +0900

    Vue.js導入

---
+++ script.js
@@ -0,0 +1,19 @@
+var app1 = new Vue({
+    el : "#app-1",
+    data : {
+        books : [
+            {
+                title:"book1",
+                class:"class1"
+            },
+            {
+                title:"book2",
+                class:"class2"
+            },
+            {
+                title:"book3",
+                class:"class3"
+            }
+        ]
+    }
+});
