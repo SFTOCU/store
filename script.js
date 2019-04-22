43526f5bc71a4004e919f9fbcabf2daf22e1f646
NonomiyaYuta <ynonomiya.ocum@gmail.com>
2019-04-22 10:27:37 +0900

    どんどん改良

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var app1 = new Vue({
-    el : "#app-1",
+var stockList = new Vue({
+    el : "#stocklist",
     data : {
         books : [
             {
@@ -8,6 +8,11 @@ var app1 = new Vue({
                 author : "author1",
                 price : 1400,
                 isbn : "0001",
+                lectures : [{
+                    name : "lecture1",
+                    youbi : "月",
+                    jigen : "１"
+                }],
                 isDetail : false
                 
             },
@@ -17,6 +22,11 @@ var app1 = new Vue({
                 author : "author2",
                 price : 1500,
                 isbn : "0002",
+                lectures : [{
+                    name : "lecture1",
+                    youbi : "月",
+                    jigen : "１"
+                }],
                 isDetail : false
             },
             {
@@ -25,8 +35,60 @@ var app1 = new Vue({
                 author : "author3",
                 price : 1600,
                 isbn : "0003",
+                lectures : [{
+                    name : "lecture1",
+                    youbi : "月",
+                    jigen : "１"
+                }],
                 isDetail : false
             }
         ]
+    },
+    methods : {
+        addToCart : function(book){
+            if(controler.cart.indexOf(book) != -1)return alert(book.title+"は既にカートに入っています。");
+            if(controler.cart.length >= 5)return alert("一度に予約できるのは5冊までです。今の予約手続きを完了させてください。");
+            if(confirm(book.title+"をカートに入れます。")){
+                controler.cart.push(book);
+                controler.totalprice += book.price;
+            }
+             
+        }
     }
 });
+
+var controler = new Vue({
+    el : "#controler",
+    data : {
+        filer : {
+            
+        },
+        cart : [],
+        totalprice : 0,
+        whichshown : 0
+    },
+    methods : {
+        deleteFromCart : function(book){
+            if(!confirm(book.title + "をカートから削除します"))return
+            this.totalprice -= book.price;
+            var index = this.cart.indexOf(book);
+            this.cart.splice(index,1);
+        }
+    }
+})
+
+for(var i = 0; i<10; i++){
+    stockList.books.push({
+                title : "book3",
+                class : "class3",
+                author : "author3",
+                price : 1600,
+                isbn : "0003",
+                lectures : [{
+                    name : "lecture1",
+                    youbi : "月",
+                    jigen : "１"
+                }],
+                isDetail : false
+            });
+}
